1c45a45d939e55d72adbde301aa1d10d16d3d1e6
Ahmad Kaif <heyahmad611@gmail.com>
2024-05-28 15:45:41 +0000

    imp

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,18 @@
+//JS - dynamically typed Language(interpreter assigns variables a type at runtime based on the variable's value at the time.)
+
+// two types
+
+// Primitive -- all call by value datatypes
+// String
+// Number
+// Boolean
+// null
+// undefined
+// Symbol
+// Bigint
+
+
+//non primitive - call by reference
+// Arrays
+// Objects
+// Function
