22d306513c41e78d83966528ee0b6431c3ee5f28
Ahmad Kaif <heyahmad611@gmail.com>
2024-05-28 15:25:18 +0000

    day-2

---
+++ 01_basics/dataTypeConversion.js
@@ -0,0 +1,17 @@
+let score1 = undefined;
+let score2 = 123;
+let score3 = true;
+let score4 = "ahmd";
+console.log(typeof(score1));
+console.log(typeof(score2));
+console.log(typeof(score3));
+console.log(typeof(score4));
+
+let value = Number(score4);
+console.log(typeof value);
+console.log(value);
+
+//"33" -> 33
+// "33abd" -> NaN
+// true -> 1 vice versa
+
