8a774ab584d4cac6c822e3474a003cee83a04de1
Ahmad Kaif <heyahmad611@gmail.com>
2024-05-29 17:01:41 +0000

    math_random_method

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,24 @@
+const num = 100;
+// console.log(num);
+// console.log(typeof num.toString());
+// console.log(num.toFixed(2));
+
+const x = 123.2345
+// console.log(x.toPrecision(4));// till 4 decimal places
+
+
+// console.log(Math.abs(-45));
+// console.log(Math.round(43.2));
+// console.log(Math.round(43.6));
+// console.log(Math.floor(43.6));
+// console.log(Math.ceil(43.6));
+// console.log(Math.max(3,4,100,23,200,12));
+
+// console.log(Math.random()); // always between 0 and 1
+// console.log(Math.floor(Math.random()*10 + 1)); // between 1 and 10
+
+
+//very important
+const max = 6;
+const min = 1;
+console.log(Math.floor(Math.random()*(max-min+1)) + min); // value between 10 and 20(min and max)
