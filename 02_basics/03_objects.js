79a9e7d8169dd438a63de5869cd06e9dfe7d01c4
Ahmad Kaif <heyahmad611@gmail.com>
2024-06-01 13:24:00 +0000

    json intro and object destructuring

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,38 @@
+const obj = {
+    name:"ahmad",
+    price:"999",
+    course:"Javascript"
+}
+
+const {name:n} = obj; // name:n --- object destructuring
+//console.log(name); 
+console.log(n);
+
+
+//react part
+// const navbar = ({company})=>{
+//     .....
+// }
+// navbar(company = "google")
+
+
+// -----------JSON and api intro ---------
+//json is an object
+// {
+//     "name" : "ahmad",   //keys are also string
+//     "courseName" : "js",
+//     "price" : "999"
+// }
+
+//sometimes we get ap in array format as well
+[
+    {},
+    {},
+    {}
+]
+
+//json formatter 
+
+
+
+
