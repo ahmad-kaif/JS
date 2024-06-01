1bcc42196b9ba1cafdf49026b7975e80321889be
Ahmad Kaif <heyahmad611@gmail.com>
2024-06-01 14:28:32 +0000

    func basics

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+//functions
+// function sayMyName(){
+//     console.log("heisenberg");
+// }
+// sayMyName();
+// sayMyName();
+// sayMyName();
+
+function login(name){
+    return `${name} just logged in`;
+}
+console.log(login("Ahmad"));
+
+
+
