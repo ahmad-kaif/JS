1a83c307d4f4af43d42088b32fd48ff1e9f6a29d
Ahmad Kaif <heyahmad611@gmail.com>
2024-06-02 08:19:21 +0000

    IIFE

---
+++ 03_basics/iife.js
@@ -0,0 +1,20 @@
+//IIFE - Immediately invoked function expressions
+// (here function definitin)();-->execution
+// (()=>{
+//     console.log("DB Connected");
+// })();
+// or
+// why iife
+// sometimes global scope creates pollution so to remove it we use iife
+// (function chai(){ //named iife
+//     console.log("DB Connected");
+// })();
+
+// ((name)=>{
+//     /un named iife
+//     console.log(`db connected ${name}`);
+// })("ahmad")
+
+
+// while writing twi IIFE's simultaneously use semi colon in first ()(); and the ()()
+
